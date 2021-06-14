da63996f56948a85b67446a9622ae6c9fab90e33
cenemif892 <85499919+cenemif892@users.noreply.github.com>
2021-06-14 07:06:15 +0000

    Create veruscoin.sh

---
+++ veruscoin.sh
@@ -0,0 +1,5 @@
+#veruscoin
+sudo apt update
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+tar xf hellminer_cpu_linux.tar.gz
+./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RXR2pH6MJXwRoxgvA2wdW4dBSRYFMix9CE.cenemif891 -p x --cpu 4
